82259bc30d9f94ae387f55de2f333b1ea26f1b3b
Rahul Gupta <73338505+rahul4793@users.noreply.github.com>
2026-01-14 16:19:08 +0530

    Remove deployment script contents

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
-set -e
-
-aws ecr get-login-password --region ap-south-1 \
-| docker login --username AWS --password-stdin 308855860105.dkr.ecr.ap-south-1.amazonaws.com
-
-docker stop backend || true
-docker rm backend || true
-
-docker pull 308855860105.dkr.ecr.ap-south-1.amazonaws.com/backend-app:latest
-
-docker run -d \
-  -p 5000:8080 \
-  --name backend \
-  308855860105.dkr.ecr.ap-south-1.amazonaws.com/backend-app:latest
+//
